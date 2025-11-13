e63b37e8579783d14eae81ed7ee7e2ad7412f3ce
brianclark <2229553982@qq.com>
2025-11-13 23:48:08 +0800

    11.13 初级cat命令，仅有可以一次性cat多个文件的功能

---
+++ mycat.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <file1> [file2 ... fileN]" << std::endl;
+        return 1;
+    }
+
+    for (int i = 1; i < argc ; ++i) {
+        std::ifstream infile(argv[i]);
+        if (!infile) {
+            std::cerr << "Error: Could not open file " << argv[i] << std::endl;
+            continue;
+        }
+
+        std::string line;
+        while (std::getline(infile, line)) {
+            std::cout << line << std::endl;
+        }
+        infile.close();
+    }
+    return 0;
+}
